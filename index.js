cc20123986fefafd89c40757d7360eaf4925a09f
Felix Mattick <felix.mattick@gmail.com>
2018-11-07 16:18:16 -0500

    Maybe fix heroku

--- index.js
+++ index.js
@@ -75,4 +75,4 @@ app.get("/application/:file", (req, res) => {
   res.send(fs.readFileSync("applications/" + req.params.file, "utf-8"));
 });
 
-app.listen(3000, () => console.log("Server started on port 3000."));
+app.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}.`));
