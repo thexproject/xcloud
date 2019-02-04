48fc8b26f92efb25b5bd6b3b7816363b144d2a99
Felix Mattick <felix.mattick@gmail.com>
2019-02-04 10:01:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(Sentry.Handlers.errorHandler());
 
 // Mongoose
 const mongoose = require("mongoose");
-mongoose.connect(`mongodb://admin:${encodeURIComponent(process.env.MONGO_PASSWORD)}@xcloud-zr62s.gcp.mongodb.net:27017/main`, { useNewUrlParser: true });
+mongoose.connect(`mongodb+srv://admin:${encodeURIComponent(process.env.MONGO_PASSWORD)}@xcloud-zr62s.gcp.mongodb.net:27017/main`, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", error => {
 	throw error;
